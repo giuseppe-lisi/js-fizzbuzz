c94a54a9a4793caad0a15391ffb1600031932058
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-10-29 11:23:51 +0100

    reso più leggibile il codice

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,11 +1,18 @@
+let printIfMultiple;
+
+// conto fino a 100
 for (let i = 1; i <= 100; i++) {
-    if (i % 5 == 0 && i % 3 == 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 == 0) {
-        console.log("Fizz");
-    } else if (i % 5 == 0) {
-        console.log("Buzz");
-    } else {
+    printIfMultiple = "";
+    // se il numero è multiplo di 3 o 5 aggiungo ad una stringa Fizz, Buzz o entrambi
+    if (i % 3 == 0) {printIfMultiple += "Fizz";}
+    if (i % 5 == 0) {printIfMultiple += "Buzz";}
+    // se il numero non era un multiplo di 3 o 5 stampo il numero,
+    // altrimenti stampo la stringa
+    if (printIfMultiple.length == false) {
         console.log(i);
+    } 
+    else {
+        console.log(printIfMultiple);
     }
 }
+
