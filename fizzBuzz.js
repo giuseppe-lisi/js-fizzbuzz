67b047dabc6dc8e8dad4f6aba8de629d0d38ea99
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-10-29 22:33:20 +0100

    migliorato con tip di alessio

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,24 +1,12 @@
-let printIfMultiple;
-
-// conto fino a 100
 for (let i = 1; i <= 100; i++) {
-    printIfMultiple = "";
-    // se il numero è multiplo di 3 o 5 aggiungo ad una stringa Fizz, Buzz o entrambi
-    if (i % 3 == 0) {printIfMultiple += "Fizz";}
-    if (i % 5 == 0) {printIfMultiple += "Buzz";}
-    // se multiplo stampo la stringa, altrimenti stampo il numero, 
-    // il tutto in dei div che vengono aggiunti alla pagina
-    if (printIfMultiple.length == false) {
+    if (i % 3 === 0 || i % 5 === 0) {
+        document.getElementById("content").innerHTML += `<div class="col bg-success py-4 border border-dark">FizzBuzz</div>`;
+    } else if (i % 3 == 0) {
+        document.getElementById("content").innerHTML += `<div class="col bg-warning py-4 border border-dark">Fizz</div>`;
+    } else if (i % 5 == 0) {
+        document.getElementById("content").innerHTML += `<div class="col bg-danger py-4 border border-dark">Buzz</div>`;
+    } else {
         document.getElementById("content").innerHTML += `<div class="col py-4 border border-dark">${i}</div>`;
-    } 
-    else {
-        if (printIfMultiple == "Fizz") {
-            document.getElementById("content").innerHTML += `<div class="col bg-warning py-4 border border-dark">${printIfMultiple}</div>`;
-        } else if (printIfMultiple == "Buzz") {
-            document.getElementById("content").innerHTML += `<div class="col bg-danger py-4 border border-dark">${printIfMultiple}</div>`;
-        } else {
-            document.getElementById("content").innerHTML += `<div class="col bg-success py-4 border border-dark">${printIfMultiple}</div>`;
-        }
     }
 }
 
